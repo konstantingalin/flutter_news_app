0e6ac0f3d4977d82761d51d922eb680544744dd1
Konstantin <the.it.promo@gmail.com>
2023-03-31 22:26:09 +0600

    Revert "add routes"
    This reverts commit f63106e62757ccc42bacb00d3061b8e64219641f.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_app/widgets/bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +8,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      bottomNavigationBar: const BottomNavBar(index: 0,),
       body: Container(
         
       ),
