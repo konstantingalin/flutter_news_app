3d2cb35442e080e23ddad44a1a6273cf78133de8
Konstantin <the.it.promo@gmail.com>
2023-03-31 22:29:40 +0600

    add bottom nav menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/widgets/bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: const BottomNavBar(index: 0,),
       body: Container(
         
       ),
