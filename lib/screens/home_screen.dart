1908525b439d60130f3cdd724d3fd13097b2700d
Konstantin <the.it.promo@gmail.com>
2023-04-01 00:02:08 +0600

    add widgets: image container and custom tag

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/models/article_model.dart';
 import 'package:flutter_news_app/widgets/bottom_nav_bar.dart';
+import 'package:flutter_news_app/widgets/custom_tag.dart';
+import 'package:flutter_news_app/widgets/image_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,10 +11,48 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      bottomNavigationBar: const BottomNavBar(index: 0,),
-      body: Container(
-        
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            )),
+      ),
+      bottomNavigationBar: const BottomNavBar(
+        index: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ImageContainer(
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            imageUrl: Article.articles[0].imageUrl,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTag(
+                  backgroundColor: Colors.grey.withAlpha(150),
+                  children: [
+                    Text(
+                      'News of the Day',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: Colors.white),
+                )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
